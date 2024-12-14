cbdc604c80b01a7311bd1e16738ea0950c9d19fb
Nicolo Veneziale <nicoloveneziale@gmail.com>
2024-12-14 15:04:31 +0000

    Added save button functionality and adjusted the cv dimensions and proportions

--- src/components/SaveFormButton.jsx
+++ src/components/SaveFormButton.jsx
@@ -1,3 +1,12 @@
+import JsPDF from "jspdf";
+
 export default function SaveFormButton() {
-  return <button>Save</button>;
+  const generatePDF = () => {
+    const report = new JsPDF("portrait", "pt", "a4");
+    report.html(document.querySelector("#cv-div")).then(() => {
+      report.save("report.pdf");
+    });
+  };
+
+  return <button onClick={generatePDF}>Save</button>;
 }
